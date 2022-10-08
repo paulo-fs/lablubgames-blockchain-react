eaba3b929710a1cdd67d9d4dc95928e4963b4d35
paulo-fs <jr.paulo87@gmail.com>
2022-10-08 10:51:59 -0300

    feat: add the trigger of start game

--- client/src/components/GameComponents/StartGame/index.tsx
+++ client/src/components/GameComponents/StartGame/index.tsx
@@ -1,13 +1,17 @@
 import {PrimaryButton} from "components";
+import { useContext } from "react";
+import { Context } from "shared/context";
 import { Container } from "./styles";
 
 export default function StartGame () {
+   const {handleStartQuestions} = useContext(Context)
+
    return (
       <Container>
          <p>
             Click <span>I'm ready</span> to start the game.
          </p>
-         <PrimaryButton>
+         <PrimaryButton onClick={handleStartQuestions}>
             I'm ready
          </PrimaryButton>
       </Container>
