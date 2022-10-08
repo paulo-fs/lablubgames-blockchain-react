9819a89ee8850b6eb2267d9dfb1c403e3065bea8
paulo-fs <jr.paulo87@gmail.com>
2022-10-08 10:48:39 -0300

    feat: created shared states and its set functions

--- client/src/shared/context/index.tsx
+++ client/src/shared/context/index.tsx
@@ -1,14 +1,56 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 
 interface ProviderPropType{
    children: ReactNode;
 }
 
-export const Context = createContext({});
+interface ContextType {
+    correctAnswerCounter: number
+    wrongAnswerCounter: number
+    questionsCounter: number
+    questionsIsStarted: boolean
+    handleStartQuestions: () => void
+    handleAnswersCounters: (value: boolean) => void
+    handleQuestionsCounter: () => void
+}
+
+export const Context = createContext({} as ContextType);
 
 export function ContextProvider({children}: ProviderPropType){
+    const [correctAnswerCounter, setCorrectAnswerCounter] = useState(0);
+    const [wrongAnswerCounter, setWrongAnswerCounter] = useState(0);
+    const [questionsCounter, setQuestionsCounter] = useState(0);
+    const [questionsIsStarted, setQuestionsIsStarted] = useState(false);
+
+    function handleStartQuestions() {
+        setQuestionsIsStarted(true);
+        setQuestionsCounter(prevState => prevState = 0);
+        setCorrectAnswerCounter(prevState => prevState = 0);
+        setWrongAnswerCounter(prevState => prevState = 0);
+    }
+
+    function handleAnswersCounters (isCorrect: boolean) {
+        if(isCorrect) {
+            setCorrectAnswerCounter(prevState => prevState++);
+        } else {
+            setWrongAnswerCounter(prevState => prevState++);
+        }
+    }
+
+    function handleQuestionsCounter() {
+        setQuestionsCounter(prevState => prevState++);
+    }
+
     return (
-        <Context.Provider value={{}}>
+        <Context.Provider value={{
+            correctAnswerCounter,
+            wrongAnswerCounter,
+            questionsCounter,
+            questionsIsStarted,
+            handleStartQuestions,
+            handleAnswersCounters,
+            handleQuestionsCounter
+        }}>
             {children}
         </Context.Provider>
     );
